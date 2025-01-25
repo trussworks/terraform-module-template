829c5e049038f7cf3ec9d5e3e0d140d0aef45cec
Barry Morrison <689591+esacteksab@users.noreply.github.com>
2025-01-25 14:00:05 -0600

    chore: pin Terraform to 1.5.7 for now

---
+++ terraform.tf
@@ -0,0 +1,11 @@
+# Last MPL Licensed Terraform version
+terraform {
+  required_version = "1.5.7"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
